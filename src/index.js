899042c51e58c07b23b7e30f7e8f7bd0d695baa5
Gabriel Felipe <gabrielfsoliveiradev@gmail.com>
2025-08-28 16:23:12 -0300

    Organiza apenas o caminho dos arquivos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import fs from 'fs'
+import path from 'path'
 
 
 fs.readdir('./meus-arquivos', 'utf-8', (err, files) => {
   if (err) throw err
-  console.log(files)
+  let ext = {}
+  files.forEach((fileName) => {
+    let file = {
+      path: path.resolve(fileName),
+      ext: path.extname(fileName)
+    }
+
+    // Verifica se o objeto ext possui a chave com o nome da extensão, caso não houver ele cria a chave e atriui um array a chave
+    if(!ext[file.ext])  ext[file.ext] = []
+    
+    // Adiciona o caminho do arquivo de acordo com a extensão feita
+    ext[file.ext].push(file.path)  
+  })
 })
