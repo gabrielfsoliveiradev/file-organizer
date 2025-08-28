3ffa1a40f0dba8fd2f33d1d7c44265a7555c25b1
Gabriel Felipe <gabrielfsoliveiradev@gmail.com>
2025-08-28 16:44:36 -0300

    Cria um arquivo para cada extensão

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 fs.readdir('./meus-arquivos', 'utf-8', (err, files) => {
   if (err) throw err
-  let ext = {}
+  let extensions = {}
   files.forEach((fileName) => {
     let file = {
       path: path.resolve(fileName),
@@ -12,9 +12,29 @@ fs.readdir('./meus-arquivos', 'utf-8', (err, files) => {
     }
 
     // Verifica se o objeto ext possui a chave com o nome da extensão, caso não houver ele cria a chave e atriui um array a chave
-    if(!ext[file.ext])  ext[file.ext] = []
+    if(!extensions[file.ext])  extensions[file.ext] = []
     
     // Adiciona o caminho do arquivo de acordo com a extensão feita
-    ext[file.ext].push(file.path)  
+    extensions[file.ext].push(file.path)  
   })
+
+  organizeFiles(extensions)
 })
+
+async function organizeFiles(extensions) {
+  const folderName = './meus-arquivos-organizados'
+  await createFolder(folderName)
+  for (const ext in extensions) {
+    await createFolder(`${folderName}/${ext}`)
+  }
+}
+
+async function createFolder(folderName) {
+  await fs.promises.mkdir(folderName, (err) => {
+    if (err){
+      console.log(`Não foi possível criar a pasta ${folderName}`)
+      throw err 
+    } 
+    console.log(`Pasta ${folderName} criada`)
+  })
+}
