87a06df0764a5354a70508a3086d3b8d08e5e66e
Gabriel Felipe <gabrielfsoliveiradev@gmail.com>
2025-08-29 16:39:09 -0300

    Usuario agora informa qual será a pasta de origem

--- copy.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'
-
-let origem = "C:\\Users\\gabri\\OneDrive\\Documentos\\GitHub\\file-organizer\\meus-arquivos\\bofebisl.png"
-let destino = "C:\\Users\\gabri\\OneDrive\\Documentos\\GitHub\\file-organizer\\bofebisl.png"
-
-fs.copyFileSync(origem, destino)
--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import fs from 'fs'
 import path from 'path'
 
+const userFolder = process.argv[2]
+if (userFolder === undefined){
+  console.log('Por favor, informe a pasta de origem')
+}else{
+  const srcPath = path.resolve(userFolder)
+  fs.readdir(srcPath, 'utf-8', (err, files) => {
+    if (err) throw err
+    let extensions = {}
+    files.forEach((fileName) => {
+      let file = {
+        path: path.resolve(srcPath, fileName),
+        ext: path.extname(fileName)
+      }
 
-fs.readdir('./meus-arquivos', 'utf-8', (err, files) => {
-  if (err) throw err
-  let extensions = {}
-  files.forEach((fileName) => {
-    let file = {
-      path: path.resolve('./meus-arquivos', fileName),
-      ext: path.extname(fileName)
-    }
+      // Verifica se o objeto ext possui a chave com o nome da extensão, caso não houver ele cria a chave e atriui um array a chave
+      if(!extensions[file.ext])  extensions[file.ext] = []
+      
+      // Adiciona o caminho do arquivo de acordo com a extensão feita
+      extensions[file.ext].push(file.path)  
+      })
 
-    // Verifica se o objeto ext possui a chave com o nome da extensão, caso não houver ele cria a chave e atriui um array a chave
-    if(!extensions[file.ext])  extensions[file.ext] = []
-    
-    // Adiciona o caminho do arquivo de acordo com a extensão feita
-    extensions[file.ext].push(file.path)  
+    organizeFiles(extensions)
   })
-
-  organizeFiles(extensions)
-})
+}
 
 function organizeFiles(extensions) {
   const folderName = path.resolve('./meus-arquivos-organizados')
@@ -35,7 +40,7 @@ function copyFile(srcPathArr, destinationPath) {
   srcPathArr.forEach((srcPath) => {
     const fileName = path.basename(srcPath)
     const finalPath = destinationPath+'/'+fileName
-    fs.copyFileSync(srcPath, finalPath)
+      fs.copyFileSync(srcPath, finalPath)
   })
 }
 
